6441ce1fbe7e5b34dde085e3418e46dddd646eb4
iamprakashkhatri <padkhu@gmail.com>
2021-01-25 21:19:37 +0545

    interaction added

--- interaction.js
+++ interaction.js
@@ -26,3 +26,49 @@
         layers:layerArray,
         view:view
     });
+
+    var dragBox = new ol.interaction.DragBox({})
+
+    dragBox.on('boxstart',function(evt){
+        console.log('Box event started')
+    })
+    dragBox.on('boxend',function(evt){
+        console.log('Box ended successfully')
+        map.getView().fit(dragBox.getGeometry().getExtent(),map.getSize());
+    })
+    // map.addInteraction(dragBox)
+
+    //Drag and drop
+
+    var dragSource = new ol.source.Vector()
+
+    var dragLayer = new ol.layer.Vector({
+        soure:dragSource
+    })
+    map.addLayer(dragLayer)
+
+    var dragDrop = new ol.interaction.DragAndDrop({
+        formatConstructors:[ol.format.GeoJSON],
+        source:dragSource
+    })
+    dragDrop.on('addfeatures',function(evt){
+        console.log('added new features')
+    })
+    // map.addInteraction(dragDrop)
+
+    var drawSource = new ol.source.Vector()
+
+    var drawLayer=new ol.layer.Vector({
+        source: drawSource
+    })
+    map.addLayer(drawLayer)
+    var draw = new ol.interaction.Draw({
+        source:drawSource,
+        // type:'Polygon'
+        type:'LineString',
+        freehand:true
+    })
+    draw.on('drawend',function(evt){
+        drawSource.clear()
+    })
+    map.addInteraction(draw)
